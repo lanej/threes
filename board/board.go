61e1b7cfa3d80a88f0d755975f857bd6332bda25
Joshua Lane <me@joshualane.com>
2016-09-15 12:40:29 -0400

    seed the board with 3,2,1

--- board/board.go
+++ board/board.go
@@ -2,6 +2,7 @@ package board
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
@@ -21,6 +22,7 @@ func newBoard(starter int) (*Board, error) {
 		},
 		deck: Deck{},
 	}
+
 	// Starter value goes in a random corner, seemingly rotates
 	startCornerRow := rand.Intn(1) * 3
 	startCornerCol := rand.Intn(1) * 3
@@ -28,13 +30,33 @@ func newBoard(starter int) (*Board, error) {
 	insertErr := board.insert(startCornerRow, startCornerCol, starter)
 
 	// Of the 8 starter tiles, there are between 1-4 {1,2,3}
+	remainingSeed := 8
+
+	seeds := []int{3, 2, 1}
+
+	for _, seed := range seeds {
+		maxSeed := math.Min(float64(3), float64(remainingSeed))
+		numSeed := rand.Intn(int(maxSeed)) + 1
+
+		for i := 0; i < numSeed; i++ {
+			empties := board.EmptyTiles()
+			index := rand.Intn(len(empties))
+
+			position := empties[index]
+
+			board.insert(position[0], position[1], seed)
+		}
+
+		remainingSeed -= numSeed
+	}
+
 	return &board, insertErr
 }
 
 func (b *Board) print() {
-	for row := 0; row < 4; row++ {
-		for col := 0; col < 4; col++ {
-			fmt.Printf("|% 4d", b.tiles[row][col])
+	for x, _ := range b.tiles {
+		for y, _ := range b.tiles[x] {
+			fmt.Printf("|% 4d", b.tiles[x][y])
 		}
 		fmt.Println("|")
 	}
@@ -46,6 +68,20 @@ type InvalidValueError struct {
 
 func (e InvalidValueError) Error() string {
 	return fmt.Sprintf("Invalid value: %d", e.Value)
+}
+
+func (b *Board) EmptyTiles() [][]int {
+	empties := make([][]int, 0)
+
+	for x, row := range b.tiles {
+		for y, v := range row {
+			if v == 0 {
+				empties = append(empties, []int{x, y})
+			}
+		}
+	}
+
+	return empties
 }
 
 func (b *Board) insert(x, y, v int) error {
--- board/board_test.go
+++ board/board_test.go
@@ -9,3 +9,12 @@ func TestBoardPrint(t *testing.T) {
 	}
 	board.print()
 }
+
+func TestBoardEmptyTiles(t *testing.T) {
+	board, _ := newBoard(96)
+	empties := cap(board.EmptyTiles())
+
+	if empties != 8 {
+		t.Errorf("Expected 8 empty tiles, got %d", empties)
+	}
+}
